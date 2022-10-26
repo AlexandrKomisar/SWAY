e2143dac5a3bdd22a614f12303895d4cc24fa317
AlexandrKomisar <monolitalex@gmail.com>
2022-10-26 12:20:31 +0300

    slider

--- js/my.js
+++ js/my.js
@@ -1,9 +1,1 @@
-const card = document.querySelectorAll(".review__slider_card");
-const slider = document.querySelector(".review__slider");
-let count = 0;
-let width;
 
-function init() {
-  console.log("resize");
-  width = document.querySelector("");
-}
